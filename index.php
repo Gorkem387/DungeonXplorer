6ded46bdd432a5d74677baa1fcaf47980acffb38
EthanCoombes <ethan.d.coombes@gmail.com>
2025-11-21 14:48:32 +0100

    connectionBDD

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    
+    <p>
+    Bienvenue sur DungeonXplorer, l'univers de dark fantasy où se mêlent aventure, stratégie et immersion
+    totale dans les récits interactifs.
+    Ce projet est né de la volonté de l’association Les Aventuriers du Val Perdu de raviver l’expérience unique
+    des livres dont vous êtes le héros. Notre vision : offrir à la communauté un espace où chacun peut
+    incarner un personnage et plonger dans des quêtes épiques et personnalisées.
+    Dans sa première version, DungeonXplorer permettra aux joueurs de créer un personnage parmi trois
+    classes emblématiques — guerrier, voleur, magicien — et d’évoluer dans un scénario captivant, tout en
+    assurant à chacun la possibilité de conserver sa progression.
+    Nous sommes enthousiastes de partager avec vous cette application et espérons qu'elle saura vous
+    plonger au cœur des mystères du Val Perdu !
+    </p>
+    <a href="inscription.php">Créer un compte</a>
+    <a href="connection.php">Se connecter</a>
+</body>
+</html>
